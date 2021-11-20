5d7937651de59c7ebb14c4b73f26c06e0f22b6fe
EricWang0827 <93813447+EricWang0827@users.noreply.github.com>
2021-11-19 19:16:24 -0800

    renaming Gaeta_etal_CLC_data.csv as Gaeta_etal_CLC_data.csv1, and modified the code accordingly to make sure read.csv work

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,4 +1,4 @@
-fish_data <- read.csv("Gaeta_etal_CLC_data.csv")#reading in gaeta data
+fish_data <- read.csv("Gaeta_etal_CLC_data1.csv")#reading in gaeta data
 library(dplyr) #loading library dplyr
 #adding column to denote fish big or small
 fish_data_cat <- fish_data %>% 
