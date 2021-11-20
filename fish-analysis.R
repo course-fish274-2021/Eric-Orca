2901d6600f43d49cfb74829455796945c69cb789
EricWang0827 <93813447+EricWang0827@users.noreply.github.com>
2021-11-19 19:23:22 -0800

    made scatterplot with length on the x-axis and scalelength on the y-axis, then color the points using lakeid to compare relationship between fish length and scalelength

--- fish-analysis.R
+++ fish-analysis.R
@@ -8,3 +8,8 @@ fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 #remove fish data with scale size smaller than 1 mm
 fish_data_cat <-filter(fish_data_cat, scalelength > 1)
+#install tidyverse and ggplot2 to make scatterplot with length on the x-axis and scalelength on the y-axis, then color the points using lakeid
+install.packages("tidyverse")
+library("ggplot2")
+ggplot(data=fish_data_cat)+
+  geom_point(mapping=aes(x=length,y=scalelength,color=lakeid))
