297001e157a14432b7b3b2e01fd339ecc7d8a624
EricWang0827 <93813447+EricWang0827@users.noreply.github.com>
2021-11-19 18:59:10 -0800

    Change size cutoff for new column

--- fish-analysis.R
+++ fish-analysis.R
@@ -3,3 +3,6 @@ library(dplyr) #loading library dplyr
 #adding column to denote fish big or small
 fish_data_cat <- fish_data %>% 
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+#change size cutoff for new column
+fish_data_cat <- fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
